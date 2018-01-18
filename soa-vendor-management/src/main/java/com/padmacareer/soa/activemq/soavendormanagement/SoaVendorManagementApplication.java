bd0ac489cd9e71e107821a2fa2574e79fa94f11e
nasre <nasre@LAPTOP-57NQC83A.fios-router.home>
2018-01-18 09:04:46 -0500

    Initial commit

---
+++ soa-vendor-management/src/main/java/com/padmacareer/soa/activemq/soavendormanagement/SoaVendorManagementApplication.java
@@ -0,0 +1,12 @@
+package com.padmacareer.soa.activemq.soavendormanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaVendorManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaVendorManagementApplication.class, args);
+	}
+}
---
+++ soa-vendor-management/src/test/java/com/padmacareer/soa/activemq/soavendormanagement/SoaVendorManagementApplicationTests.java
@@ -0,0 +1,16 @@
+package com.padmacareer.soa.activemq.soavendormanagement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoaVendorManagementApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
